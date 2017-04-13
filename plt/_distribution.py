ba4ff7f6a0316cab7fdcfe49d6a8e6872bd5b8b6
Erik Teichmann <erik-teichmann@gmx.de>
2017-04-13 13:17:45 +0200

    Add plots for results

---
+++ plt/_distribution.py
@@ -0,0 +1,104 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+from scipy.special import erf
+import csv
+import os
+
+# All our data-files are saved in the folder data
+os.chdir('../data')
+files = os.listdir('.')
+
+
+# This is the list, where all our data will be saved in
+data_list = []
+
+# We only want our data-files, not some random hidden file
+for element in files:
+    if element[0] == '.':
+        files.remove(element)
+
+# Now we get all our different parameters
+for element in files:
+    flip = int(element[0])
+    cooling_parameter = float(element[2:10])
+    data_list.append([flip, cooling_parameter])
+    with open(element, 'r') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=' ')
+        for row in csv_reader:
+            data_list[-1].append(float(row[1]))
+
+data_list.sort(key=lambda tup: tup[1])
+
+# -----------------------------------------------------------------------------
+# Plot
+# -----------------------------------------------------------------------------
+
+l_min = 4292.14
+
+
+def Cumulative(x):
+    return 0.5*(1 + erf(x/np.sqrt(2)))
+
+
+def Errf(length, mean, sterr):
+    return (Cumulative((length - mean)/sterr)) / \
+           (1 - Cumulative((l_min - mean)/sterr))
+
+
+def Distribution(x, mean, sterr):
+    return 1/(sterr*np.sqrt(2*np.pi))*np.exp(-(x - mean)*(x - mean) /
+                                            (2*sterr*sterr))
+
+
+def GaussianDistribution(length, mean, sterr):
+    variance = sterr*sterr
+    return 1/np.sqrt(2*np.pi*variance)*np.exp(-(length - mean) *
+                    (length - mean)/(2*variance))*2/(np.sqrt(variance) *
+                    (1 - erf((l_min - mean)/np.sqrt(2*variance))))
+
+
+flip_1 = np.array([x[1:] for x in data_list if x[0] == 1])
+flip_2 = np.array([x[1:] for x in data_list if x[0] == 2])
+
+#print(flip_1)
+
+fig, ax = plt.subplots()
+ax.set_xlabel('Weglänge [km]')
+ax.set_ylabel('Wahrscheinlichkeit')
+ax.grid()
+for element in flip_1:
+    values = np.sort(element[1:])
+    points = values.size
+    values, indices = np.unique(values, return_counts=True)
+    cumul = np.cumsum(indices/points)
+    popt, pcov = curve_fit(Errf, values, cumul, [4500, 1000])
+    x = np.linspace(l_min, 6500, 500)
+    ax.plot(x, GaussianDistribution(x, popt[0], popt[1]),
+            label=r'$C=%.3f, \mu=%.1f, \sigma=%.1f$' % (element[0], popt[0], popt[1]))
+
+ax.set_xlim(4000, 6500)
+ax.set_ylim(0., 4*1e-5)
+ax.legend(loc='best')
+fig.tight_layout()
+fig.savefig('../plt/distribution_1.pdf', dpi=300)
+
+fig, ax = plt.subplots()
+ax.set_xlabel('Weglänge [km]')
+ax.set_ylabel('Wahrscheinlichkeit')
+ax.grid()
+for element in flip_2:
+    values = np.sort(element[1:])
+    points = values.size
+    values, indices = np.unique(values, return_counts=True)
+    cumul = np.cumsum(indices/points)
+    popt, pcov = curve_fit(Errf, values, cumul, [4500, 1000])
+    x = np.linspace(l_min, 6500, 500)
+    ax.plot(x, GaussianDistribution(x, popt[0], popt[1]),
+            label=r'$C=%.3f, \mu=%.1f, \sigma=%.1f$' % (element[0], popt[0], popt[1]))
+
+ax.set_xlim(4000, 6500)
+ax.set_ylim(0., 4*1e-5)
+ax.legend(loc='best')
+fig.tight_layout()
+fig.savefig('../plt/distribution_2.pdf', dpi=300)
---
+++ plt/_time.py
@@ -0,0 +1,71 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+import csv
+import os
+
+# All our data-files are saved in the folder data
+os.chdir('../data')
+files = os.listdir('.')
+
+
+# This is the list, where all our data will be saved in
+data_list = []
+
+# We only want our data-files, not some random hidden file
+for element in files:
+    if element[0] == '.':
+        files.remove(element)
+
+# Now we get all our different parameters
+for element in files:
+    with open(element, 'r') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=' ')
+        time_sum = 0
+        line_count = 0
+        for row in csv_reader:
+            time_sum += float(row[0])
+            line_count += 1.
+        mean_time = time_sum/line_count
+        flip = int(element[0])
+        cooling_parameter = float(element[2:10])
+        data_list.append([flip, cooling_parameter, mean_time])
+
+# -----------------------------------------------------------------------------
+# Plot
+# -----------------------------------------------------------------------------
+
+
+def FitFunc(x, a, b):
+    return a*np.tan(b*x)
+
+
+flip_1 = [[x[1], x[2]*1e-6] for x in data_list if x[0] == 1]
+flip_2 = [[x[1], x[2]*1e-6] for x in data_list if x[0] == 2]
+
+flip_1 = np.transpose(np.array(flip_1))
+flip_2 = np.transpose(np.array(flip_2))
+
+popt_1, pcov = curve_fit(FitFunc, flip_1[0], flip_1[1])
+popt_2, pcov = curve_fit(FitFunc, flip_2[0], flip_2[1])
+
+fig, ax = plt.subplots()
+
+x = np.linspace(0.45, 1., 500, endpoint=False)
+
+ax.set_xlabel('Cooling-Parameter')
+ax.set_ylabel('Simulationsdauer [s]')
+
+ax.plot(x, FitFunc(x, popt_1[0], popt_1[1]), label=r'$%.2f\tan(%.2f x)$' %
+                                                   (popt_1[0], popt_1[1]))
+ax.plot(x, FitFunc(x, popt_2[0], popt_2[1]), label=r'$%.2f\tan(%.2f x)$' %
+                                                   (popt_1[0], popt_1[1]))
+ax.plot(flip_1[0], flip_1[1], 'o', label='Typ 1')
+ax.plot(flip_2[0], flip_2[1], 'o', label='Typ 2')
+
+ax.set_xlim(0.45, 1.)
+ax.set_ylim(0., 25.)
+ax.legend(loc='best')
+
+os.chdir('../plt')
+fig.savefig('time.pdf', dpi=300)
